54ec9fa7c03cdea9ccaf12de2fcb0f7bbff4ac25
Ali Sufyan <alisufyan327@gmail.com>
2022-08-05 17:58:10 +0500

    input form included

--- with-jest-app/pages/index.tsx
+++ with-jest-app/pages/index.tsx
@@ -1,16 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import {useState} from 'react'
+
 
 import styles from '@/pages/index.module.css'
 
 export default function Home() {
+  const [integer,setInteger]=useState(0)
   return (
+
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <form action="">
+          <label htmlFor="">Enter Integer :</label>
+          <input onChange={(e: any)=>{setInteger(e.target.value)}} className={styles.formInput} type="text" value={integer} />
+          <h6>{integer}</h6>
+         </form>
     </div>
   )
 }
