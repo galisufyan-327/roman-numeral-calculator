9126309cb5dfa6ff554873d9a2a73583cf244c76
Ali Sufyan <alisufyan327@gmail.com>
2022-08-05 17:55:36 +0500

    Next app created

---
+++ with-jest-app/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import styles from '@/pages/index.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
+  )
+}
