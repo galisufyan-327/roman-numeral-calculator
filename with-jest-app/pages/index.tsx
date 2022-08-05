36e706e07043cc81ad137ed741631d3586658379
Ali Sufyan <alisufyan327@gmail.com>
2022-08-05 18:53:04 +0500

    some more styles are added into input and output

--- with-jest-app/pages/index.tsx
+++ with-jest-app/pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '@/pages/index.module.css'
 
 export default function Home() {
 
-  const [roman,setRoman]=useState('')
+  const [roman, setRoman]=useState('')
 
 
   return (
@@ -16,12 +16,16 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome to Integer to Roman Calculator</h1>
+      <h4>Welcome to Integer to Roman Calculator</h4>
       <form action="">
           <label htmlFor="">Enter Integer :</label>
           <input onChange={(e: any)=>{setRoman(int2roman(e.target.value))}} className={styles.formInput}
            type="number"  min="1" max="3999" />
-          <h6>{roman}</h6>
+
+           <div className={styles.output}>
+           {roman}
+           </div>
+        
          </form>
     </div>
   )
