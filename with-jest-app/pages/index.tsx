e8e3bcdd10829bfa795835e9b2d93be0ad508b9c
Ali Sufyan <alisufyan327@gmail.com>
2022-08-10 11:24:05 +0500

    unwanted imports removed from index.tsx

--- with-jest-app/pages/index.tsx
+++ with-jest-app/pages/index.tsx
@@ -1,9 +1,4 @@
-import Head from 'next/head'
-import {useState} from 'react'
-import { int2roman } from './Int2Romant'
 import Form from './Form'
-
-
 import styles from '@/pages/index.module.css'
 
 export default function Home() {
